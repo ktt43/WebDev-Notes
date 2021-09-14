8d31eee7e87ac27c6bae36468096a78fe3f2605b
ktt43 <ktt43@rutgers.edu>
2021-09-14 16:45:50 -0400

    some exersices to 09

--- javascript-exercises-main/javascript-exercises-main/08_calculator/calculator.js
+++ javascript-exercises-main/javascript-exercises-main/08_calculator/calculator.js
@@ -1,24 +1,68 @@
-const add = function() {
+const add = function(x,y) {
+  if(x===undefined){
+    x=0;
+  }
+  if(y===undefined){
+    y=0;
+  }
+  return x+y;
 	
 };
 
-const subtract = function() {
+const subtract = function(x,y) {
+  if(x===undefined){
+    x=0;
+  }
+  if(y===undefined){
+    y=0;
+  }
+	
+  return x-y;
+};
+
+const sum = function(array) {
+  let sum=0;
+  if(array.length==0){
+    return sum;
+  } else {
+    array.forEach(num=> {
+      sum+=num;
+    })
+  }
+	
+  return sum;
+};
+
+const multiply = function(array) {
+    let product=1;
+
+    array.forEach(num=>{
+      product*=num;
+    })
+
+    return product;
+};
+
+const power = function(x,y) {
+  let original=x;
+  for(;y>1;y--){
+    x*=original;
+  }
+  return x;
 	
 };
 
-const sum = function() {
-	
-};
+const factorial = function(x) {
+  if(x==0){
+    return 1;
+  }
 
-const multiply = function() {
+  let result=1;
+  for(;x>=1;x--){
+    result*=x;
+  }
 
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
+  return result;
 	
 };
 
--- javascript-exercises-main/javascript-exercises-main/09_palindromes/palindromes.js
+++ javascript-exercises-main/javascript-exercises-main/09_palindromes/palindromes.js
@@ -1,5 +1,22 @@
-const palindromes = function () {
+//remove spaces and punctuation then lowercase 
 
+const palindromes = function (word) {
+    word = word.toLowerCase();
+    const regex = /[a-z]/g;
+    const found = word.match(regex);
+    word = found.join('');
+
+    let length = word.length-1;
+    let result = "";
+
+    while(length>=0){
+        result+=found[length];
+        length--;
+    }
+    if(word==result)
+    return true;
+
+    return false;
 };
 
 module.exports = palindromes;
